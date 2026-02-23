d090ec0405959a1fbc69a8b2c94c08b5efcd766c
Amanbairagi17 <amanbairagi741@gmail.com>
2026-02-23 19:52:12 +0530

    [Aman] : added feature UC4 Extended Unit Support

--- src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppTest.java
+++ src/test/java/com/app/quantitymeasurement/QuantityMeasurementAppTest.java
@@ -56,4 +56,47 @@ public class QuantityMeasurementAppTest {
             () -> new QuantityLength(1.0, null)
         );
     }
+        
+        @Test
+        void testEquality_YardToFeet() {
+            assertEquals(
+                new QuantityLength(1.0, LengthUnit.YARDS),
+                new QuantityLength(3.0, LengthUnit.FEET)
+            );
+        }
+
+        @Test
+        void testEquality_YardToInches() {
+            assertEquals(
+                new QuantityLength(1.0, LengthUnit.YARDS),
+                new QuantityLength(36.0, LengthUnit.INCH)
+            );
+        }
+
+        @Test
+        void testEquality_CentimeterToInch() {
+            assertEquals(
+                new QuantityLength(1.0, LengthUnit.CENTIMETERS),
+                new QuantityLength(0.393701, LengthUnit.INCH)
+            );
+        }
+
+        @Test
+        void testEquality_MultiUnit_Transitive() {
+            QuantityLength a = new QuantityLength(1.0, LengthUnit.YARDS);
+            QuantityLength b = new QuantityLength(3.0, LengthUnit.FEET);
+            QuantityLength c = new QuantityLength(36.0, LengthUnit.INCH);
+
+            assertEquals(a, b);
+            assertEquals(b, c);
+            assertEquals(a, c);
+        }
+
+        @Test
+        void testEquality_NonEquivalent() {
+            assertNotEquals(
+                new QuantityLength(1.0, LengthUnit.CENTIMETERS),
+                new QuantityLength(1.0, LengthUnit.FEET)
+            );
+    }
 }
